88baa796de59d0664ba15950047b13f6b10b955b
Tobias Olea <tobiolea97@gmail.com>
2026-03-14 15:51:22 -0300

    Add error handling and not found pages for invoices
    - Introduced a new error component to display error messages and allow users to retry actions.
    - Created a 404 Not Found page for invoices, enhancing user experience when an invoice is not found.
    - Updated the invoice editing page to utilize the notFound function for better error handling when an invoice is missing.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,5 +1,6 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { notFound } from 'next/navigation';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
@@ -9,6 +10,11 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         fetchInvoiceById(id),
         fetchCustomers(),
       ]);
+
+    if (!invoice) {
+    notFound();
+    }
+
     return (
         <main>
             <Breadcrumbs
