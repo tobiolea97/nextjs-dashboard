c4e889cb82dcc1f92683482e26b8aaf02d140df5
Tobias Olea <tobiolea97@gmail.com>
2026-03-16 07:56:59 -0300

    Remove error throwing in deleteInvoice function to allow successful deletion of invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -118,7 +118,6 @@ export async function updateInvoice(
 // --------------------------- Delete Invoice
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
